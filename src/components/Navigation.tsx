fadfcf74a8470b3b8680cdd79057ae699b288f83
Gudmundur Fridgeirsson <gudmundurfridgeirsson@Mac.c.is>
2025-08-04 15:41:35 +0000

    Make logo even larger for better visibility

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -39,8 +39,8 @@ export default function Navigation() {
             <img
               src={logo}
               alt="GF Training logo"
-              className="h-10 w-auto object-contain drop-shadow"
-              style={{ maxWidth: 120 }}
+              className="h-12 w-auto object-contain drop-shadow"
+              style={{ maxWidth: 150 }}
             />
           </Link>
           
